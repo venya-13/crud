cd182a62d58fddb9407e8e1e76229a4311cadfb1
venya-13 <venya.vyrovoj>
2025-04-15 15:39:48 +0200

    push

--- internal/postgres-db/models/models.go
+++ internal/postgres-db/models/models.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	Id      uint   `json:"id" gorm:"primaryKey"`
-	Name    string `json:"name"`
-	Surname string `json:"surname"`
+	Id      uint
+	Name    string
+	Surname string
 }
