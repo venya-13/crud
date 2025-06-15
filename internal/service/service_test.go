3cfb8715e9fa623c968340be5b9e6a1a9a1b89f8
venya-13 <venya.vyrovoj>
2025-06-15 03:26:07 +0200

    service_test, getuserbyid mock

---
+++ internal/service/service_test.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type mockDB struct{}
+
+func (m *mockDB) GetUserById(id string) ([]User, error) {
+	return []User{
+		{Id: 1, Name: "John", Surname: "Doe"},
+	}, nil
+}
+
+func (m *mockDB) CreateUser(name, surname string) (uint, error) { return 0, nil }
+func (m *mockDB) GetAllUsers() ([]User, error)                  { return nil, nil }
+func (m *mockDB) UpdateUser(id string, user User) ([]User, error) {
+	return nil, nil
+}
+func (m *mockDB) DeleteUser(id string) error { return nil }
+func (m *mockDB) Close()                     {}
+
+func TestGetUserById(t *testing.T) {
+	db := &mockDB{}
+	svc := New(db, nil)
+
+	user, err := svc.GetUserById("1")
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(user)) // check is there one user or more
+	assert.Equal(t, "John", user[0].Name)
+	assert.Equal(t, "Doe", user[0].Surname)
+}
